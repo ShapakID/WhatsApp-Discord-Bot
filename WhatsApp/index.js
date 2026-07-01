487578ab8658ccef5870b3aec7324d3c1ff41730
ShapakID <shafairunizarne@gmail.com>
2026-07-01 19:33:55 +0700

    Fix autoread not working by awaiting it

--- WhatsApp/index.js
+++ WhatsApp/index.js
@@ -307,7 +307,7 @@ async function hydroInd() {
 
             const m = kay
             if (global.autoread && m.key && !m.key.fromMe) {
-                hydro.readMessages([m.key]).catch(() => { });
+                await hydro.readMessages([m.key]);
             }
             require('./hydro')(hydro, m, chatUpdate, store)
         } catch (err) {
